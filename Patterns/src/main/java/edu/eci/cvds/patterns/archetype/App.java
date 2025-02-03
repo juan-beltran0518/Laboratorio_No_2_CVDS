74683b0d97e6dbac655099210575676257584389
David Salomon Baena Rubio <dsbaenar@gmail.com>
2025-02-03 14:35:23 -0500

    Esqueleto del proyecto realizado

--- Patterns/src/main/java/edu/eci/cvds/patterns/archetype/App.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/archetype/App.java
@@ -1,16 +1,28 @@
 package edu.eci.cvds.patterns.archetype;
 
+import java.util.Scanner;
+import edu.eci.cvds.patterns.shapes.ShapeMain;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+         
         if (args.length > 0) {
-            String fullName = String.join(" ", args);
-            System.out.println("Hello " + fullName + "!");
-        } else {
-            System.out.println("Hello World!");
+            ShapeMain.main(args);
+        } 
+        
+        else {
+            System.out.println("No se proporcionaron argumentos.");
+            System.out.print("Ingrese un argumento: ");
+            String userInput = scanner.nextLine(); // Leer entrada del usuario
+
+            // Ejecutar ShapeMain con el argumento ingresado
+            ShapeMain.main(new String[]{userInput});
         }
+
+        scanner.close(); // Cerrar el Scanner
     }
 }
-
---
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/RegularShapeType.java
@@ -0,0 +1,6 @@
+package edu.eci.cvds.patterns.shapes;
+
+public enum RegularShapeType {
+	Triangle, Quadrilateral, Pentagon, Hexagon
+}
+
