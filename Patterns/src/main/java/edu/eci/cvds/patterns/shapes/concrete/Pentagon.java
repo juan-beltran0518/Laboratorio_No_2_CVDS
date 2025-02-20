f762b3840954bf7e9b8daab58183ec82cd78caa1
David Salomón Baena Rubio <68250833+DSBAENAR@users.noreply.github.com>
2025-02-20 13:40:06 -0500

    Documentación actualizada

--- Patterns/src/main/java/edu/eci/cvds/patterns/shapes/concrete/Pentagon.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/concrete/Pentagon.java
@@ -2,10 +2,16 @@ package edu.eci.cvds.patterns.shapes.concrete;
 
 import edu.eci.cvds.patterns.shapes.Shape;
 
-public class Pentagon implements Shape{
-	public int getNumberOfEdges() {
-		return 5;
-	}
+/**
+ * Representa un pentágono, una figura geométrica de cinco lados.
+ */
+public class Pentagon implements Shape {
+    /**
+     * Retorna el número de lados del pentágono.
+     *
+     * @return Número de lados del pentágono (5).
+     */
+    public int getNumberOfEdges() {
+        return 5;
+    }
 }
-
-
