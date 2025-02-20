0dd7316acce666349c1a06a24aff461744f407d7
David Salomón Baena Rubio <68250833+DSBAENAR@users.noreply.github.com>
2025-02-20 13:43:37 -0500

    Documentación actualizada

--- Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
@@ -1,33 +1,38 @@
 package edu.eci.cvds.patterns.shapes;
 
 import edu.eci.cvds.patterns.shapes.concrete.*;
-public class ShapeFactory{
 
-	public static Shape create(RegularShapeType type) {
-		switch (type) {
-		case Triangle:
-			Triangle t = new Triangle();
-			return t;
-			
-		
-		case Quadrilateral:
-			Quadrilateral q = new Quadrilateral();
-			return q;
-			
-		case Pentagon:
-			Pentagon p = new Pentagon();
-			return p;
-			
-		case Hexagon:
-			Hexagon h = new Hexagon();
-			return h;
-			
-		default:
-			break;
-		
-		}
-		return null;
-	}
+/**
+ * Fábrica para la creación de formas geométricas regulares.
+ */
+public class ShapeFactory {
+    /**
+     * Crea una instancia de una forma geométrica basada en el tipo especificado.
+     *
+     * @param type Tipo de forma geométrica a crear.
+     * @return Una instancia de {@link Shape}, o {@code null} si el tipo no es válido.
+     */
+    public static Shape create(RegularShapeType type) {
+        switch (type) {
+            case Triangle:
+                Triangle t = new Triangle();
+                return t;
 
+            case Quadrilateral:
+                Quadrilateral q = new Quadrilateral();
+                return q;
 
+            case Pentagon:
+                Pentagon p = new Pentagon();
+                return p;
+
+            case Hexagon:
+                Hexagon h = new Hexagon();
+                return h;
+
+            default:
+                break;
+        }
+        return null;
+    }
 }
