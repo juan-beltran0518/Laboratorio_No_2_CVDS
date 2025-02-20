9438ce813453589a95da708d01fd3e986bcf6e6f
David Salomón Baena Rubio <68250833+DSBAENAR@users.noreply.github.com>
2025-02-20 13:42:47 -0500

    Documentación actualizada

--- Patterns/src/main/java/edu/eci/cvds/patterns/shapes/RegularShapeType.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/RegularShapeType.java
@@ -1,6 +1,8 @@
 package edu.eci.cvds.patterns.shapes;
 
+/**
+ * Representa los tipos de formas geométricas regulares disponibles.
+ */
 public enum RegularShapeType {
-	Triangle, Quadrilateral, Pentagon, Hexagon
+    Triangle, Quadrilateral, Pentagon, Hexagon
 }
-
