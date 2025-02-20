d8241100458a2ed22cec758636f8bd1767669462
David Salomón Baena Rubio <68250833+DSBAENAR@users.noreply.github.com>
2025-02-20 13:36:56 -0500

    Documentación actualizada

--- Patterns/src/main/java/edu/eci/cvds/patterns/archetype/App.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/archetype/App.java
@@ -4,25 +4,28 @@ import java.util.Scanner;
 import edu.eci.cvds.patterns.shapes.ShapeMain;
 
 /**
- * Hello world!
+ * Clase principal que ejecuta la aplicación.
  */
 public class App {
+    /**
+     * Método principal que inicia la ejecución del programa.
+     * Si se proporcionan argumentos en la línea de comandos, los pasa a `ShapeMain`.
+     * Si no hay argumentos, solicita al usuario ingresar uno manualmente.
+     *
+     * @param args Argumentos proporcionados en la ejecución del programa.
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-         
+
         if (args.length > 0) {
             ShapeMain.main(args);
-        } 
-        
-        else {
+        } else {
             System.out.println("No se proporcionaron argumentos.");
             System.out.print("Ingrese un argumento: ");
-            String userInput = scanner.nextLine(); // Leer entrada del usuario
-
-            // Ejecutar ShapeMain con el argumento ingresado
+            String userInput = scanner.nextLine(); 
             ShapeMain.main(new String[]{userInput});
         }
 
-        scanner.close(); // Cerrar el Scanner
+        scanner.close(); 
     }
 }
