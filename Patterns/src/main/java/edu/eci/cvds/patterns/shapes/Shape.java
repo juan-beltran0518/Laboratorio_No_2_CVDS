1b9000d0e70c95e94a71df57f8684839e7c18d88
David Salomón Baena Rubio <68250833+DSBAENAR@users.noreply.github.com>
2025-02-20 13:43:11 -0500

    Documentación actualizada

--- Patterns/src/main/java/edu/eci/cvds/patterns/shapes/Shape.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/Shape.java
@@ -1,6 +1,13 @@
 package edu.eci.cvds.patterns.shapes;
 
+/**
+ * Interfaz que define una forma geométrica regular.
+ */
 public interface Shape {
-	public int getNumberOfEdges();
-
+    /**
+     * Obtiene el número de lados de la forma geométrica.
+     *
+     * @return Número de lados de la forma.
+     */
+    public int getNumberOfEdges();
 }
